451073f9034b3d352a0f444107ce8f7d192b7d9c
jorgegordillo88 <jorge.jgordillo@mercadolibre.com.co>
2021-08-18 18:47:42 -0500

    servicios OS

---
+++ src/main/java/com/mercadolibre/rampup/rampup/RampupApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.rampup.rampup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RampupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RampupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolibre/rampup/rampup/RampupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.rampup.rampup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RampupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
